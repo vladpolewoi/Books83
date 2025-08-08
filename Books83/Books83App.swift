336e787c2a67c7411331592f890305540b8a412a
vlad polevoi <vlad.polewoi1@gmail.com>
2025-08-08 16:49:40 +0300

    Initial Commit

---
+++ Books83/Books83App.swift
@@ -0,0 +1,17 @@
+//
+//  Books83App.swift
+//  Books83
+//
+//  Created by Quest76 on 08.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct Books83App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
